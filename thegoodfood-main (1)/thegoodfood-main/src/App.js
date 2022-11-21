6cc0dc07f494afba7e46fdee1746247b654d735a
Harsimar <78412292+harsimar288@users.noreply.github.com>
2022-11-21 14:18:23 +0530

    Add files via upload

---
+++ thegoodfood-main (1)/thegoodfood-main/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/contact/Footer";
+import Download from "./components/download/Download";
+import Header from "./components/header/Header";
+import Menu from "./components/menu/Menu";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <About />
+      <Download />
+      <Menu />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
